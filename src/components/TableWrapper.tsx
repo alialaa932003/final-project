c4724d7290d94eb47c6e8d2475d7282bee23d4d4
Salah Mohamed <salah.mohamed0404@gmail.com>
2025-04-11 18:41:46 +0200

    refactor TableWrapper component to support additional props and improve styling logic

--- src/components/TableWrapper.tsx
+++ src/components/TableWrapper.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import Pagination from "./Pagination";
+import { cn } from "@/lib/utils";
 
 interface TableWrapperProps {
    children: React.ReactNode;
    totalPages?: number;
+   className?: string;
+   noContainerStyle?: boolean;
 }
-const TableWrapper = ({ children, totalPages }: TableWrapperProps) => {
+const TableWrapper = ({
+   children,
+   totalPages,
+   className,
+   noContainerStyle,
+}: TableWrapperProps) => {
    return (
-      <div className="h-full space-y-4 rounded-2xl bg-gray-0 p-5 shadow-md">
+      <div
+         className={cn(
+            !noContainerStyle &&
+               "h-full space-y-4 rounded-2xl bg-gray-0 p-5 shadow-md",
+            className,
+         )}
+      >
          {children}
          {totalPages && <Pagination pageCount={totalPages} />}
       </div>
