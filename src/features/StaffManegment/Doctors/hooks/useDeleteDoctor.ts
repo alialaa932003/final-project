d0870f3bf9f602362b9a0d955d11a68ef3955207
Salah Mohamed <salah.mohamed0404@gmail.com>
2025-04-18 18:57:30 +0200

    refactor(useDeleteDoctor): replace custom mutation with useOptimisticMutation for improved consistency

--- src/features/StaffManegment/Doctors/hooks/useDeleteDoctor.ts
+++ src/features/StaffManegment/Doctors/hooks/useDeleteDoctor.ts
@@ -1,44 +1,15 @@
-import useCustomMutation from "@/hooks/useCustomMutation";
+import { QUERY_KEYS } from "@/constants";
+import { useOptimisticMutation } from "@/hooks/useOptimisticMutation";
 import { deleteDoctor } from "@/services/staff/doctors/deleteDoctor";
-import { useQueryClient } from "@tanstack/react-query";
-import { useTranslation } from "react-i18next";
-import { toast } from "react-toastify";
 
 export const useDeleteDoctor = () => {
-   const { t } = useTranslation("staff");
-   const queryClient = useQueryClient();
-   const mutationResult = useCustomMutation(deleteDoctor, {
-      onMutate: async (newDoctor) => {
-         await queryClient.cancelQueries({ queryKey: ["doctors"] });
-
-         const previousDoctors = queryClient.getQueryData([
-            "doctors",
-         ]) as StaffApiResponseWithPagination<Doctor[]>;
-
-         queryClient.setQueryData(
-            ["doctors"],
-            (old: StaffApiResponseWithPagination<Doctor[]>) => ({
-               ...old,
-               data: {
-                  ...old.data,
-                  items: old?.data.items.filter(
-                     (doctor) => doctor.id !== newDoctor.id,
-                  ),
-               },
-            }),
-         );
-
-         return { previousDoctors };
-      },
-      onSuccess: () => {
-         toast.success(t("doctor-deleted-successfully"));
-      },
-      onError: (error) => {
-         console.error(error);
-         toast.error(t("doctor-deleted-failed"));
-      },
-      onSettled: () => {
-         queryClient.invalidateQueries({ queryKey: ["doctors"] });
+   const mutationResult = useOptimisticMutation({
+      mutationFn: deleteDoctor,
+      queryKey: [QUERY_KEYS.DOCTORS],
+      dataPath: ["data", "items"],
+      options: {
+         successMessageKey: "staff:doctor-deleted-successfully",
+         errorMessageKey: "staff:doctor-deletion-failed",
       },
    });
 
