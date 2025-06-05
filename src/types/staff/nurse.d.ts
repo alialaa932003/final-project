54f775060c3b29999937a6d51360f0403d78bba6
Salah Mohamed <salah.mohamed0404@gmail.com>
2025-06-05 15:53:40 +0300

    feat: add Nurse interface to define nurse properties

---
+++ src/types/staff/nurse.d.ts
@@ -0,0 +1,11 @@
+interface Nurse {
+   id: string;
+   first_name: string;
+   last_name: string;
+   email: string;
+   phone: string;
+   is_active: number;
+   clinic: Clinic;
+   created_at: string;
+   updated_at: string;
+}
