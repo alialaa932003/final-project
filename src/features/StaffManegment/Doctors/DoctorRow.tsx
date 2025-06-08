df6c810ee5262a311b4e53cbee0120e6d984c766
Salah Mohamed <salah.mohamed0404@gmail.com>
2025-06-08 17:24:19 +0300

    refactor: change doctor to table view

---
+++ src/features/StaffManegment/Doctors/DoctorRow.tsx
@@ -0,0 +1,31 @@
+import { Badge } from "@/components/ui/badge";
+import { TableCell, TableRow } from "@/components/ui/table";
+import ActionsMenu from "./ActionsMenu";
+
+type DoctorRowProps = {
+   doctor: Doctor;
+   rowNumber: number;
+};
+
+function DoctorRow({ doctor, rowNumber }: DoctorRowProps) {
+   const fullName = `${doctor.first_name} ${doctor.last_name}`;
+
+   return (
+      <TableRow>
+         <TableCell>{rowNumber}</TableCell>
+         <TableCell>{fullName}</TableCell>
+         <TableCell>{doctor.email}</TableCell>
+         <TableCell>{doctor.phone}</TableCell>
+         <TableCell>
+            <Badge colorTheme={doctor.specialization?.color}>
+               {doctor.specialization?.name}
+            </Badge>
+         </TableCell>
+         <TableCell className="flex items-center justify-end gap-2">
+            <ActionsMenu id={doctor.id} />
+         </TableCell>
+      </TableRow>
+   );
+}
+
+export default DoctorRow;
--- src/types/staff/doctor.d.ts
+++ src/types/staff/doctor.d.ts
@@ -4,7 +4,6 @@ interface Doctor {
    last_name: string;
    email: string;
    phone: string;
-   profile_picture: string;
    specialization: Specialization;
    created_at: string;
    updated_at: string;
