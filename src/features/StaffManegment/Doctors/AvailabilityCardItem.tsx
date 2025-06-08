428a38fe736410440cdf95ac38f090d98bb48b03
Salah Mohamed <salah.mohamed0404@gmail.com>
2025-06-08 22:56:09 +0300

    feat: add AvailabilityCardItem component to display doctor availability details

---
+++ src/features/StaffManegment/Doctors/AvailabilityCardItem.tsx
@@ -0,0 +1,51 @@
+import dayjs from "dayjs";
+import { Separator } from "@/components/ui/separator";
+import { getDayjsFromTime } from "@/utils/getDayjsFromTime";
+
+type AvailabilityCardItemProps = {
+   availability: Availability;
+};
+
+function AvailabilityCardItem({ availability }: AvailabilityCardItemProps) {
+   const preparedFrom = getDayjsFromTime(availability.from).format("HH:mm A");
+   const preparedTo = getDayjsFromTime(availability.to).format("HH:mm A");
+   const preparedDate = dayjs(availability.date).format("DD/MM/YYYY");
+
+   return (
+      <li className="rounded-xl bg-primary-500/90 px-8 py-6 text-white">
+         <div className="grid grid-cols-3 gap-4 px-1">
+            <div className="space-y-1">
+               <h4 className="font-semibold text-gray-50">From:</h4>
+               <p className="text-xl">{preparedFrom}</p>
+            </div>
+            <Separator
+               orientation="vertical"
+               className="h-12 justify-self-center"
+            />
+            <div className="space-y-1">
+               <h4 className="font-semibold text-gray-50">To:</h4>
+               <p className="text-xl">{preparedTo}</p>
+            </div>
+         </div>
+
+         <Separator className="my-4" />
+
+         <div className="grid grid-cols-3 items-center gap-4 px-1">
+            <div className="space-y-1">
+               <h4 className="font-semibold text-gray-50">Date:</h4>
+               <p className="text-xl">{preparedDate}</p>
+            </div>
+            <Separator
+               orientation="vertical"
+               className="h-12 justify-self-center"
+            />
+            <div className="space-y-1">
+               <h4 className="font-semibold text-gray-50">Clinic:</h4>
+               <p className="text-xl">{availability.clinic.name}</p>
+            </div>
+         </div>
+      </li>
+   );
+}
+
+export default AvailabilityCardItem;
